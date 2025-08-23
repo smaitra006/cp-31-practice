097386ae25eeae2cc128c58ec386f9078971f8c1
Subhrajit Maitra <smaitra006@gmail.com>
2025-08-23 21:02:57 +0530

    Update

--- 900/C_Vasilije_in_Cacak.cpp
+++ 900/C_Vasilije_in_Cacak.cpp
@@ -1,38 +1,39 @@
+// Competitive Programming Template (C++)
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define vi vector<int>
-#define vll vector<long long>
-#define pii pair<int, int>
+#define int long long
 #define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define all(x) (x).begin(), (x).end()
+#define all(v) v.begin(), v.end()
+#define endl '\n'
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+const int MOD = 1e9+7;
+const int INF = 1e18;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
 
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        ll n, k, x;
-        cin >> n >> k >> x;
-        ll min_sum = (k * (k + 1)) / 2;
-        ll max_sum = ((n * (n + 1)) / 2) - (((n - k) * (n - k + 1)) / 2);
-        if (x >= min_sum && x <= max_sum)
-        {
-            cout << "YES" << endl;
-        }
-        else
-        {
-            cout << "NO" << endl;
-        }
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return a / gcd(a, b) * b; }
+
+void solve() {
+    // Your logic here
+    int n, k, x;
+    cin >> n >> k >> x;
+    int min_sum = (k * (k + 1)) / 2;
+    int max_sum = ((n * (n + 1)) / 2) - (((n - k) * (n - k + 1)) / 2);
+    if (x >= min_sum && x <= max_sum) {
+        cout << "YES" << endl;
     }
+    else {
+        cout << "NO" << endl;
+    }
+}
 
+int32_t main() {
+    fast_io
+    int t = 1;
+    cin >> t; // Uncomment if multiple test cases
+    while(t--) solve();
     return 0;
 }
