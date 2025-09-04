b2d4b63a8ceb812483d1aa0f8405017f1cc339d2
Subhrajit Maitra <smaitra006@gmail.com>
2025-09-04 22:32:53 +0530

    Update

---
+++ Practice/A_Split_it.cpp
@@ -0,0 +1,47 @@
+// Competitive Programming Template (C++)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define endl '\n'
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return a / gcd(a, b) * b; }
+
+void solve()
+{
+    // Your logic here
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+
+    int l = 0, r = s.length();
+    int cnt = 0;
+    while (l < r - 2 && s[l] == s[r - 1]) {
+        cnt++;
+        l++;
+        r--;
+    }
+    if (cnt >= k) {
+        cout << "YES" << endl;
+    }
+    else cout << "NO" << endl;
+}
+
+int32_t main()
+{
+    fast_io
+        int t = 1;
+    cin >> t; // Uncomment if multiple test cases
+    while (t--) solve();
+    return 0;
+}
