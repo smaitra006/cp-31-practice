410ad5af82f4e3abd57b6bebda8e2b5382bb767d
Subhrajit Maitra <smaitra006@gmail.com>
2025-09-29 21:41:37 +0530

    Update

---
+++ Practice/A_George_and_Accommodation.cpp
@@ -0,0 +1,45 @@
+// Competitive Programming Template (C++)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define endl '\n'
+#define fast_io            \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0);
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return a / gcd(a, b) * b; }
+
+void solve()
+{
+  // Your logic here
+  int n;
+  cin >> n;
+  int cnt = 0;
+  while (n--)
+  {
+    int p, q;
+    cin >> p >> q;
+    if ((q - p) >= 2)
+      cnt++;
+  }
+  cout << cnt << endl;
+}
+
+int32_t main()
+{
+  fast_io int t = 1;
+  // cin >> t; // Uncomment if multiple test cases
+  while (t--)
+    solve();
+  return 0;
+}
