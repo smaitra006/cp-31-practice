02e4aedd2ec46c7b5691d3827904fac6d9a83d47
Subhrajit Maitra <smaitra006@gmail.com>
2025-10-12 00:07:49 +0530

    Update

---
+++ 1000/B_MEXor_Mixup.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- TYPE ALIASES & CONSTANTS ---
+using ll = long long;
+using ld = long double;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using pii = pair<ll, ll>;
+const ll MOD = 1e9 + 7;
+const ll INF = 2e18;
+const int MAXN = 2e5 + 5;
+
+// --- MACROS & UTILITIES ---
+#define fast_io                \
+  ios::sync_with_stdio(false); \
+  cin.tie(NULL);               \
+  cout.tie(NULL);
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) (ll) v.size()
+#define fori(i, a, b) for (ll i = a; i < b; ++i)
+#define ford(i, a, b) for (ll i = a; i >= b; --i)
+#define precision(x) cout << fixed << setprecision(x)
+
+// --- FUNCTION UTILITIES ---
+ll gcd(ll a, ll b)
+{
+  while (b)
+  {
+    a %= b;
+    swap(a, b);
+  }
+  return a;
+}
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+ll power(ll base, ll exp)
+{
+  ll res = 1;
+  base %= MOD;
+  while (exp > 0)
+  {
+    if (exp % 2 == 1)
+      res = (res * base) % MOD;
+    base = (base * base) % MOD;
+    exp /= 2;
+  }
+  return res;
+}
+
+ll modInverse(ll n)
+{
+  return power(n, MOD - 2);
+}
+
+// --- DEBUG MACRO ---
+#ifdef LOCAL_DEBUG
+#define debug(x) cerr << #x << " = " << x << endl
+#else
+#define debug(x)
+#endif
+
+int prefix_xor(int n)
+{
+  int rem = n % 4;
+  if (rem == 0)
+    return n;
+  if (rem == 1)
+    return 1;
+  if (rem == 2)
+    return n + 1;
+  // if (rem == 3)
+  return 0;
+}
+
+void solve()
+{
+  // Your logic here
+  int a, b;
+  cin >> a >> b;
+  int curr_xor = prefix_xor(a - 1);
+  if (curr_xor == b)
+    cout << a << endl;
+  else if ((curr_xor ^ b) == a)
+    cout << a + 2 << endl;
+  else
+    cout << a + 1 << endl;
+}
+
+int main()
+{
+  fast_io
+      // #ifdef LOCAL_DEBUG
+      // freopen("input.txt", "r", stdin);
+      // freopen("output.txt", "w", stdout);
+      // #endif
+
+      ll t = 1;
+  cin >> t; // Uncomment if multiple test cases
+  while (t--)
+    solve();
+
+  return 0;
+}
---
+++ 1000/C_Double_ended_Strings.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =============================================================================
+//  TYPE ALIASES
+// =============================================================================
+using ll = long long;
+using ld = long double;
+using pii = pair<ll, ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+// =============================================================================
+//  CONSTANTS
+// =============================================================================
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// =============================================================================
+//  MACROS
+// =============================================================================
+#define fast_io                \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(v) ((ll)(v).size())
+#define fi first
+#define se second
+#define endl '\n'
+
+// =============================================================================
+//  UTILITY FUNCTIONS
+// =============================================================================
+inline ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+inline ll power(ll base, ll exp, ll mod = MOD)
+{
+  ll res = 1;
+  base %= mod;
+  while (exp > 0)
+  {
+    if (exp & 1)
+      res = (res * base) % mod;
+    base = (base * base) % mod;
+    exp >>= 1;
+  }
+  return res;
+}
+
+// =============================================================================
+//  SOLVE FUNCTION
+// =============================================================================
+void solve()
+{
+  string a, b;
+  cin >> a >> b;
+  int max_len = 0;
+  for (int i = 0; i < a.length(); ++i)
+  {
+    for (int j = i; j < a.length(); ++j)
+    {
+      string sub = a.substr(i, j - i + 1);
+      if (b.find(sub) != string::npos)
+      {
+        if ((int)sub.length() > max_len)
+        {
+          max_len = sub.length();
+        }
+      }
+    }
+  }
+  cout << (a.length() - max_len) + (b.length() - max_len) << endl;
+}
+
+// =============================================================================
+//  MAIN FUNCTION
+// =============================================================================
+int main()
+{
+  fast_io;
+  // #ifdef LOCAL
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+  // #endif
+
+  ll t = 1;
+  cin >> t;
+  while (t--)
+    solve();
+}
