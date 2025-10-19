5b075def775d53f0c21d83e7cbbaed47d27366fb
Subhrajit Maitra <smaitra006@gmail.com>
2025-10-19 21:30:56 +0530

    Update

---
+++ 1100/B_Li_Hua_and_Pattern.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =============================================================================
+//  TYPE ALIASES
+// =============================================================================
+using ll = long long;
+using ld = long double;
+using pii = pair<ll, ll>;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+// =============================================================================
+//  CONSTANTS
+// =============================================================================
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// =============================================================================
+//  MACROS
+// =============================================================================
+#define fast_io                \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(v) ((ll)(v).size())
+#define fi first
+#define se second
+#define endl '\n'
+
+// =============================================================================
+//  UTILITY FUNCTIONS
+// =============================================================================
+inline ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+inline ll power(ll base, ll exp, ll mod = MOD)
+{
+  ll res = 1;
+  base %= mod;
+  while (exp > 0)
+  {
+    if (exp & 1)
+      res = (res * base) % mod;
+    base = (base * base) % mod;
+    exp >>= 1;
+  }
+  return res;
+}
+
+// =============================================================================
+//  SOLVE FUNCTION
+// =============================================================================
+void solve()
+{
+  int n, k;
+  cin >> n >> k;
+  vvi a(n, vi(n));
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      cin >> a[i][j];
+    }
+  }
+  int cnt = 0;
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      if (a[i][j] != a[n - i - 1][n - j - 1])
+        cnt++;
+    }
+  }
+  cnt /= 2;
+  if (cnt > k)
+    cout << "NO" << endl;
+  else
+  {
+    k -= cnt;
+    if (n & 1)
+      cout << "YES" << endl;
+    else if (k & 1)
+      cout << "NO" << endl;
+    else
+      cout << "YES" << endl;
+  }
+}
+
+// =============================================================================
+//  MAIN FUNCTION
+// =============================================================================
+int main()
+{
+  fast_io;
+  // #ifdef LOCAL
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+  // #endif
+
+  ll t = 1;
+  cin >> t;
+  while (t--)
+    solve();
+}
